803975a723499ec149f71de89be12ac007f74534
Jeffrey Carpenter <spaz926@me.com>
2019-04-26 10:36:56 -0500

    Implemented adding new movie to the list

--- Movie List/View Controllers/AddMovieViewController.swift
+++ Movie List/View Controllers/AddMovieViewController.swift
@@ -8,17 +8,34 @@
 
 import UIKit
 
-class AddMovieViewController: UIViewController {
+class AddMovieViewController: UIViewController, MovieControllerProtocol {
+    
+    var movieController: MovieController?
 
     @IBOutlet weak var movieTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func submitButtonPressed(_ sender: UIButton) {
+        
+        // Ensure VC has a movieController, and movieTextField is not empty or nil
+        guard let movieController = movieController,
+        let movieName = movieTextField.text,
+        !movieName.isEmpty
+        else { return }
+        
+        // Add movie to list
+        movieController.create(movieName: movieName)
+        
+        // Clear the movieTextField
+        movieTextField.text = ""
+        
+        #warning("Test Messages")
+        print("Added movie \(movieController.movies.last?.name)")
+        
     }
     
 
