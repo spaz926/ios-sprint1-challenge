3b28a6360f441c7dd4e320c2dca2723c4205d281
Jeffrey Carpenter <spaz926@me.com>
2019-04-26 10:17:36 -0500

    Created VC files and connected IBOutlets and IBActions

---
+++ Movie List/View Controllers/AddMovieViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddMovieViewController.swift
+//  Movie List
+//
+//  Created by Jeffrey Carpenter on 4/26/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class AddMovieViewController: UIViewController {
+
+    @IBOutlet weak var movieTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func submitButtonPressed(_ sender: UIButton) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Movie List/View Controllers/MoviesTabBarViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MoviesTabBarViewController.swift
+//  Movie List
+//
+//  Created by Jeffrey Carpenter on 4/26/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MoviesTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
